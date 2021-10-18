a4725fa3807bfc2a835b43e0247833201d44d168
manikantanynala97 <90610801+manikantanynala97@users.noreply.github.com>
2021-10-18 12:47:56 +0530

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,22 @@
+// program to check if the string is palindrome or not
+
+function checkPalindrome(str) {
+
+    
+    let length = string.length;
+
+    // loop through half of the string
+    for (let i = 0; i < len / 2; i++) {
+
+        // check if first and last string are same
+        if (string[i] !== string[len - 1 - i]) {
+            return ''It is not a palindrome'';
+        }
+    }
+    return ''It is a palindrome'';
+}
+
+
+let value = checkPalindrome(string);
+
+console.log(value);
