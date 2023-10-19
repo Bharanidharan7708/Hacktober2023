50018e4f06324f37fcd60af49505c69709e5f5d0
Bharani Dharan <52794516+Bharanidharan7708@users.noreply.github.com>
2023-10-19 15:37:00 +0530

    added binary search (#85)

---
+++ BinarySearch.py
@@ -0,0 +1,12 @@
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1  # Target not found
+print(binary_search([1,2,3,4,5], 3))
