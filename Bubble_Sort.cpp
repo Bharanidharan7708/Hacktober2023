8012505df734f98abb7060713cd82215146de077
sulaemanahmed <132727787+sulaemanahmed@users.noreply.github.com>
2023-10-31 23:01:35 -0500

    Update Bubble_Sort.cpp
    Added modularity and improved readability in the C++ code by separating input, display, and bubble sort logic into separate functions. This makes the code more organized and easier to maintain.

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -1,44 +1,53 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-  //declaring array
-  int array[5];
-  cout<<"Enter 5 numbers randomly : "<<endl;
-  for(int i=0; i<5; i++)
-  {
-    //Taking input in array
-    cin>>array[i];
-  }
-  cout<<endl;
-  cout<<"Input array is: "<<endl;
-
-  for(int j=0; j<5; j++)
-  {
-    //Displaying Array
-    cout<<"\t\t\tValue at "<<j<<" Index: "<<array[j]<<endl;
-  }
-  cout<<endl;
-  // Bubble Sort Starts Here
-  int temp;
-  for(int i2=0; i2<=4; i2++)
-  {
-    for(int j=0; j<4; j++)
-    {
-      //Swapping element in if statement
-      if(array[j]>array[j+1])
-      {
-        temp=array[j];
-        array[j]=array[j+1];
-        array[j+1]=temp;
-      }
+void inputArray(int arr[], int size) {
+    cout << "Enter " << size << " numbers randomly: " << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
     }
-  }
-  // Displaying Sorted array
-  cout<<"  Sorted Array is: "<<endl;
-  for(int i3=0; i3<5; i3++)
-  {
-    cout<<"\t\t\tValue at "<<i3<<" Index: "<<array[i3]<<endl;
-  }
-  return 0;
 }
+
+void displayArray(int arr[], int size) {
+    cout << "Input array is: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << "\t\t\tValue at " << i << " Index: " << arr[i] << endl;
+    }
+}
+
+void bubbleSort(int arr[], int size) {
+    int temp;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void displaySortedArray(int arr[], int size) {
+    cout << "Sorted Array is: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << "\t\t\tValue at " << i << " Index: " << arr[i] << endl;
+    }
+}
+
+int main() {
+    int array[5];
+    const int size = 5;
+
+    inputArray(array, size);
+    cout << endl;
+    displayArray(array, size);
+
+    bubbleSort(array, size);
+
+    cout << endl;
+    displaySortedArray(array, size);
+
+    return 0;
+}
+
