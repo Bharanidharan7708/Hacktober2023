f9aadc509ea07a2226a3ff1ee3f8ded2cb9080eb
Bharani Dharan <52794516+Bharanidharan7708@users.noreply.github.com>
2024-03-06 17:13:41 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,20 @@
-Development Focus for the Year Ahead:
+from pymilvus import connections
+from pymilvus import utility
+from pymilvus import Collection
 
-In the rapidly evolving landscape where the world is increasingly embracing machine learning (ML) and data science, I am committed to aligning my professional development with these transformative trends. As part of the Emerging Technologies department, I recognize the paramount importance of staying ahead of the curve in these fields.
 
-My primary focus for the upcoming year revolves around immersing myself in ML and data science initiatives. I firmly believe that these skills will not only enhance my capabilities but also position me as a valuable asset to our organization and society. Through hands-on projects and targeted learning, I aim to harness the power of ML and data science to drive innovation and create tangible impact.
+connections.connect(          #yo connect with db
+    host='20.244.48.175',
+    port='19530'
+)
 
-I am excited about the opportunities that lie ahead and remain dedicated to embracing the challenges of this dynamic landscape, leveraging ML and data science to propel our organization forward in an increasingly data-driven world.
+utility.list_collections() #use this line to display the collections inside the db, our collection name is ITC_ITCHOTELS_scrapped
+
+collection = Collection("ITC_ITCHOTELS_scrapped")
+
+collection.schema #to get the schema
+
+query_result = collection.query(output_fields=["id", "text"], limit=20, expr="")
+
+for result in query_result:
+    print(result)
