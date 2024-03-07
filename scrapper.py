d71d7c56ccb253f2abd428e1886b93856add0fa7
Bharani Dharan <52794516+Bharanidharan7708@users.noreply.github.com>
2024-03-07 04:34:33 +0000

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,20 +1,26 @@
-from pymilvus import connections
-from pymilvus import utility
-from pymilvus import Collection
-
-
-connections.connect(          #yo connect with db
-    host='20.244.48.175',
-    port='19530'
-)
-
-utility.list_collections() #use this line to display the collections inside the db, our collection name is ITC_ITCHOTELS_scrapped
-
-collection = Collection("ITC_ITCHOTELS_scrapped")
-
-collection.schema #to get the schema
-
-query_result = collection.query(output_fields=["id", "text"], limit=20, expr="")
-
-for result in query_result:
-    print(result)
+import openai
+from langchain.text_splitter import TokenTextSplitter
+ 
+azure_api = "https://sravanakumar13sathish.openai.azure.com/"
+api_key = "f93979cbf9894257affd4fee8b4e08fb"
+api_version = "2023-03-15-preview"
+EMBEDDING_MODEL = "text-embedding-ada-002"
+TEXT_COMPLETION_MODEL = 'Policy_GPT'
+CHAT_COMPLETION_MODEL = 'gpt-35-turbo'
+openai.api_base = azure_api
+openai.api_key = api_key
+openai.api_type = 'azure'
+openai.api_version = api_version
+ 
+os.environ["OPENAI_API_TYPE"] = "azure"
+os.environ["OPENAI_API_BASE"] = azure_api
+os.environ["OPENAI_API_KEY"] = api_key
+os.environ["OPENAI_API_VERSION"] = api_version
+ 
+   embeddings = OpenAIEmbeddings(
+            deployment=EMBEDDING_MODEL,
+            model="text-embedding-ada-002",
+            openai_api_base=azure_api,
+            openai_api_type="azure",
+            chunk_size=1
+        )
