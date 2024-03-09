aa174e58332dd85e4b0cfc4ba9c4c3363f4fcabf
Bharani Dharan <52794516+Bharanidharan7708@users.noreply.github.com>
2024-03-09 14:52:09 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,24 +1,23 @@
+import locationtagger
 from langdetect import detect, detect_langs
+from deep_translator import GoogleTranslator #pip install -U deep-translator
+
+
+def city(text): 
+    place_entity = locationtagger.find_locations(text = text)
+    return place_entity.cities
  
-def detect_language(sentence):
-    detected_lang = detect(sentence)
-    return detected_lang
+def translate(sentence, target_lang):
+    sentence = GoogleTranslator(source='auto', target= target_lang ).translate(sentence) 
+    return sentence
  
-def get_language_name(language_code):
-    # You can use a language mapping dictionary to get the language name from the language code
-    language_mapping = {
-        'en': 'English',
-        'fr': 'French',
-        'es': 'Spanish',
-        'hi':'Hindi',
-        'pa':'Punjabi',
-        # Add more language codes and names as needed
-    }
-    return language_mapping.get(language_code, 'Unknown')
+def detect_lang(sentence):
+    detected_lang = detect_langs(sentence)
+    lang_str = str(detected_lang[0])
+    return lang_str[:2]
+
  
-# Example usage
-sentence = "ਤੁਸੀ ਕਿਵੇਂ ਹੋ"
-detected_language = detect_language(sentence)
-print (detected_language)
-language_name = get_language_name(detected_language)
-print(f"The detected language is: {language_name}")
+sentence = "hey hi"
+lang = detect_lang(sentence)
+context = translate(sentence, 'hi') #for example I'm translating it in Hindi
+
